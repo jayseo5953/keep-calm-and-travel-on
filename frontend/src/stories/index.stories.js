4f3c7d52c54cc5faec91b8cf62fed93dea45da5e
Porson <porsonlee@gmail.com>
2020-02-17 16:45:37 -0800

    added nav bar and loading animation stories

--- frontend/src/stories/index.stories.js
+++ frontend/src/stories/index.stories.js
@@ -10,11 +10,19 @@ import SaveTrip from "../components/SaveTrip"
 import Submit from "../components/Submit"
 import AddButton from "../components/AddButton"
 import EditButton from "../components/EditButton"
+import CircularDeterminate from "../components/CircularDeterminate"
+import ButtonAppBar from "../components/ButtonAppBar"
 
-// Search Stories
 
+// Nav Bar Stories
+
+storiesOf("ButtonAppBar", module) 
+  .add("ButtonAppBar", () => <ButtonAppBar></ButtonAppBar>)
+
+// SearchBar Stories
 storiesOf("SearchBar", module)
   .add("Display", () => <SearchBar onChange={action()}></SearchBar>)
+
 
 
 // Button Stories
@@ -25,6 +33,13 @@ storiesOf("Buttons", module)
   .add("Submit", () => <Submit onClick={action()}></Submit>)
   .add("AddButton", () => <AddButton onClick={action()}></AddButton>)
   .add("EditButton", () => <EditButton onClick={action()}></EditButton>)
+
+
+// Loading Animation Stories
+storiesOf("Loading Animations", module)
+  .add("CircularDeterminate", () => <CircularDeterminate></CircularDeterminate>)
   
-storiesOf("TheMainEvent", module)
+
+// The Main Event Stories
+storiesOf("The Main Event", module)
   .add("Slider", () => <CustomizedSlider></CustomizedSlider>)
