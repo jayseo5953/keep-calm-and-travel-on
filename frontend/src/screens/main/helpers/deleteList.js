0642ce6c24d248a15800b0f70fe61708a1e19c39
Jay Seo <sjs5953@hotmail.com>
2020-02-18 22:43:00 -0800

    deleting list send list of items back to list of activities

--- frontend/src/screens/main/helpers/deleteList.js
+++ frontend/src/screens/main/helpers/deleteList.js
@@ -1,5 +1,18 @@
 export default function deleteList (columnId, columns, setColumns) {
   let newState = {...columns};
+  let deletedItems = newState[columnId].items
   delete newState[columnId];
+  newState['list'].items = [...deletedItems, ...newState['list'].items]
   setColumns(newState)
+  // console.log({
+  //   ...newState['list'],
+  //   items: [...newState['list'].items, ...deletedItems] 
+  // })
+
+  // setColumns(
+    // {
+    //   ...newState['list'],
+    //   items: [...newState['list'].items, ...deletedItems] 
+    // }
+  // )
 }
