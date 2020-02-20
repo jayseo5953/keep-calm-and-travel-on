d8c0d923c1bb29122a2aed8dcaffd6ea6585d090
tausvels <engr.tausifkhan@gmail.com>
2020-02-19 21:34:54 -0800

    fixed some errors with the map markers

--- frontend/src/components/TheMainEvent/Map.jsx
+++ frontend/src/components/TheMainEvent/Map.jsx
@@ -1,40 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
-import * as activityData from '../../mockData.json';
 require('dotenv').config();
 
 const Map = (props) => {
-  let activityData2 = props.activities;
+  let activityData = props.activities;
   let initialCenter=props.initialCenter;
-  console.log(initialCenter)
 
   const [selectedActivity, setSelectedActivity] = useState(null);
+  
   if (initialCenter.length === 0) {
-    return (
-      'NOTHING TO SHOW'
-    )
+    return ('NO MAP FOUND')
   } else {
-    // console.log(activityData2[0])
     return (
       <GoogleMap
         defaultZoom={11}
         defaultCenter={{lat: Number(initialCenter[0].lat), lng:  Number(initialCenter[0].long)}}
       >
-        {activityData2.map(activity => {
-      
+        {activityData.map(activity => {
           return(
-          <Marker
-            key={activity.id}
-            position={{
-              lat: Number(activity.lat),
-              lng: Number(activity.long)
-            }}
-            onClick={() => {
-              setSelectedActivity(activity);
-            }}
-            
-          />)
-        })}
+            <Marker
+              key={activity.id}
+              position={{
+                lat: Number(activity.lat),
+                lng: Number(activity.long)
+              }}
+              onClick={() => {
+                setSelectedActivity(activity);
+              }}
+              
+            />)
+          })}
   
         {selectedActivity && (
           <InfoWindow
@@ -54,17 +49,17 @@ const Map = (props) => {
         )}
       </GoogleMap>
     );
-
   }
 }
 const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function GMap (props) {
+  const mapUrl = process.env.REACT_APP_GMAPURL;
   const mapApiKey = process.env.REACT_APP_GMAPKey;
   return (
     <div style={{ width: "60vw", height: "60vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${mapApiKey}`}
+        googleMapURL={`${mapUrl}=${mapApiKey}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
