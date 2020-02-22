baeed1c362255e93ad22023b1f5a8d02cb509d72
tausvels <engr.tausifkhan@gmail.com>
2020-02-21 19:48:38 -0800

    barebone of the backend for post

---
+++ backend/repository/trips_repository.js
@@ -0,0 +1,13 @@
+module.exports = (db) => {
+  return {
+
+    postTrips: () => {
+      console.log('Inside the trips repository')
+    },
+
+    getTrips: () => {
+      console.log('Inside the trips repository')
+    }
+
+  }
+}
