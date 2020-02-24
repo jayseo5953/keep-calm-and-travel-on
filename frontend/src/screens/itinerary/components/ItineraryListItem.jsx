611ee4e235e945e6e712aa09ed99aae8ca81c804
Porson <porsonlee@gmail.com>
2020-02-23 16:51:15 -0800

    fixed activity alignment

--- frontend/src/screens/itinerary/components/ItineraryListItem.jsx
+++ frontend/src/screens/itinerary/components/ItineraryListItem.jsx
@@ -8,18 +8,20 @@ const ItineraryListItem = (props) => {
     <div className="itinerary-grid">
       <div className="city-name">
        <img src={props.imageUrl} width="200px" height ="200px"/>
-       <div>
-        <h2>{props.city} , {props.country}</h2>
-       </div>
+        <div>
+          <h2>{props.city} , {props.country}</h2>
+        </div>
       </div>
       <div class="profile">
         <h3>Trip for: {props.firstName} {props.lastName}</h3>
       </div>
       <div className="itinerary-info">
         <h2>{props.scheduleName}</h2>
-        <p><strong>Activity: </strong>{props.name}</p>
-        <p><strong>Price: </strong>{props.priceCent}</p>
-        <p><strong>Time of Operation: </strong>{props.timeOperation}</p>
+        <div className="activity-info">
+          <p><strong>Activity: </strong>{props.name}</p>
+          <p><strong>Price: </strong>{props.priceCent}</p>
+          <p><strong>Time of Operation: </strong>{props.timeOperation}</p>
+        </div>
       </div>
       <div className="itinerary-total-cost">
         <h4>Total cost of your trip: ${props.totalCost}</h4>
