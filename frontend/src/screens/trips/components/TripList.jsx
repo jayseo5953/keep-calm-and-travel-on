583e224a11eda8813133da009305f6d7c48cf181
tausvels <engr.tausifkhan@gmail.com>
2020-02-25 23:04:02 -0800

    cleaned code base for the Trip component and all the children, made a helper function called deleteTrips

--- frontend/src/screens/trips/components/TripList.jsx
+++ frontend/src/screens/trips/components/TripList.jsx
@@ -1,57 +1,56 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import TripListItem from './TripListItem'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import TripListItem from "./TripListItem";
+import Button from "@material-ui/core/Button";
 
-import DeleteButton from './DeleteButton'
-import EditButton from './EditButton'
+import DeleteButton from "./DeleteButton";
+import EditButton from "./EditButton";
 
-import './trip.css';
+import "./trip.css";
 
-import axios from 'axios'
+import deleteTrip from '../helpers/deleteTrips';
 
-function deleteTrip (trip, trips, setTrips) {
-
-  let tripId = trip.id;
-
-  let copy = [...trips];
-
-  let newState = copy.filter(trip => trip.id !== tripId);
-  console.log('new State: ',newState)
-
-  setTrips(newState);
-
-  axios.post(`/trips/delete`, {
-    tripId
-  })
-}
-
-const TripList = (props) => {
+const TripList = props => {
   return (
     <div className="grid-container">
-
-      {!props.trips.length? 
-      <div className='no-trips'>
-        <h2 style={{textAlign:"center", fontWeight:"bold", marginBottom:"50px"}}> You have no saved Trips!</h2>
-        <Button variant="contained" color="primary" >
-          <h4 style={{ fontWeight:"bold"}}>
-            <Link to='/'>Start Tripper</Link>
-          </h4> 
-        </Button>
-      </div>
-      :""}
-      {props.trips.map(trip =>{
-       return (
-        <div className='trip-item'>
-          <div className='trip-info'>
-            <TripListItem trip={trip} />
+      {!props.trips.length ? (
+        <div className="no-trips">
+          <h2
+            style={{
+              textAlign: "center",
+              fontWeight: "bold",
+              marginBottom: "50px"
+            }}
+          >
+            {" "}
+            You have no saved Trips!
+          </h2>
+          <Button variant="contained" color="primary">
+            <h4 style={{ fontWeight: "bold" }}>
+              <Link to="/">Start Tripper</Link>
+            </h4>
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+      {props.trips.map(trip => {
+        return (
+          <div className="trip-item">
+            <div className="trip-info">
+              <TripListItem trip={trip} />
+            </div>
+            <div className="buttons-container">
+              <EditButton trip={trip} />
+              <DeleteButton
+                deleteTrip={deleteTrip}
+                trip={trip}
+                trips={props.trips}
+                setTrips={props.setTrips}
+              />
+            </div>
           </div>
-          <div className="buttons-container">
-            <EditButton trip={trip} />
-            <DeleteButton deleteTrip={deleteTrip} trip={trip} trips={props.trips} setTrips={props.setTrips} />
-          </div>
-         </div>
-         ) 
+        );
       })}
     </div>
   );
--- frontend/src/screens/trips/components/TripListItem.jsx
+++ frontend/src/screens/trips/components/TripListItem.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
-import { useHistory } from "react-router-dom"
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
 
-const TripListItem = (props) => {
-  
-  let history = useHistory(); 
+const TripListItem = props => {
+  let history = useHistory();
   return (
     <div className="grid-itinerary">
       <div>
-        <Button onClick={()=>{history.push(`/itinerary/${props.trip.id}/${props.trip.country}/${props.trip.city}`)}}><span className='trip-name'><strong>{props.trip.trip_name}</strong></span></Button>
-        <h5 className='trip-destination'>
-          <span className='trip-city'>Total: ${props.trip.total_cost} ({props.trip.city}, {props.trip.country}) </span>
+        <Button
+          onClick={() => {
+            history.push(
+              `/itinerary/${props.trip.id}/${props.trip.country}/${props.trip.city}`
+            );
+          }}
+        >
+          <span className="trip-name">
+            <strong>{props.trip.trip_name}</strong>
+          </span>
+        </Button>
+        <h5 className="trip-destination">
+          <span className="trip-city">
+            Total: ${props.trip.total_cost} ({props.trip.city},{" "}
+            {props.trip.country}){" "}
+          </span>
         </h5>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default TripListItem 
+export default TripListItem;
