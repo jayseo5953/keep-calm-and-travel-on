76f0b524c28c06972307dbb38290f033b1292042
tausvels <engr.tausifkhan@gmail.com>
2020-02-25 22:40:17 -0800

    cleaned the code base for the main component, change the LINK tag in the FriendList component to an a tag to get rid of the console errors

--- frontend/src/screens/main/components/Card.jsx
+++ frontend/src/screens/main/components/Card.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Draggable } from 'react-beautiful-dnd'
-import deleteCard from '../helpers/deleteCard'
-const Card = (props) => {
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import deleteCard from "../helpers/deleteCard";
+
+const Card = props => {
   return (
     <Draggable
       key={props.draggableId}
@@ -9,62 +10,73 @@ const Card = (props) => {
       index={props.index}
     >
       {(provided, snapshot) => {
-      return (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className='card'
-          style={{
-            // backgroundImage: `url(${props.item.image_url})`,
-            // backgroundRepeat: 'no-repeat',
-            backgroundSize: 'contain',
-            // backgroundRepeat: 'no-repeat',
-            userSelect: "none",
-            backgroundColor: snapshot.isDragging
-            // ? "rgba(58, 59, 74, 1)"
-            // : "rgba(72, 90, 134, 0.8)",
-            ? "rgba(50, 50, 54, 1)"
-            : "rgba(50, 50, 50, 0.9)",
-            ...provided.draggableProps.style
-          }}
-        >
-          <div className='item-info'
-            onMouseOver={()=> {
-              if (props.droppableId !== 'list' ) {
-                // console.log(props.item.id)
-                props.setHoverActivity(props.item.id)
-              }
-              else {
-                return null;
-              }
-            }}
-            onMouseLeave={()=> {
-              if (props.droppableId !== 'list' ) {
-                // console.log(props.item.id)
-                props.setHoverActivity(null)
-              }
-              else {
-                return null;
-              }
+        return (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className="card"
+            style={{
+              backgroundSize: "contain",
+              userSelect: "none",
+              backgroundColor: snapshot.isDragging
+                ? "rgba(50, 50, 54, 1)"
+                : "rgba(50, 50, 50, 0.9)",
+              ...provided.draggableProps.style
             }}
           >
-            <div className='item-cont'>
-                <img className='item-photo' src={`${props.item.image_url}`} alt="activty"/> 
-                <strong className='item-name'>{props.item.name}</strong>
-            </div>
-            <div className='item-cont2'>
-              <span className='item-time'>{props.item.time_operation} </span>
-              <strong className='item-price'>$
-              {props.item.price_cents/100}</strong> 
-              {props.droppableId ==='list'?"": <button className='item-delete btn btn-danger' onClick={()=> deleteCard(props.index, props.droppableId, props.columns, props.setColumns)}><i className="material-icons">
-              close
-              </i></button>} 
+            <div
+              className="item-info"
+              onMouseOver={() => {
+                if (props.droppableId !== "list") {
+                  props.setHoverActivity(props.item.id);
+                } else {
+                  return null;
+                }
+              }}
+              onMouseLeave={() => {
+                if (props.droppableId !== "list") {
+                  props.setHoverActivity(null);
+                } else {
+                  return null;
+                }
+              }}
+            >
+              <div className="item-cont">
+                <img
+                  className="item-photo"
+                  src={`${props.item.image_url}`}
+                  alt="activty"
+                />
+                <strong className="item-name">{props.item.name}</strong>
+              </div>
+              <div className="item-cont2">
+                <span className="item-time">{props.item.time_operation} </span>
+                <strong className="item-price">
+                  ${props.item.price_cents / 100}
+                </strong>
+                {props.droppableId === "list" ? (
+                  ""
+                ) : (
+                  <button
+                    className="item-delete btn btn-danger"
+                    onClick={() =>
+                      deleteCard(
+                        props.index,
+                        props.droppableId,
+                        props.columns,
+                        props.setColumns
+                      )
+                    }
+                  >
+                    <i className="material-icons">close</i>
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      );
-    }}
+        );
+      }}
     </Draggable>
   );
 };
--- frontend/src/screens/main/components/DndContext.jsx
+++ frontend/src/screens/main/components/DndContext.jsx
@@ -1,79 +1,79 @@
-import React from 'react';
-import { DragDropContext } from 'react-beautiful-dnd'
-import CardList from './CardList'
-import addCardList from '../helpers/addCardList'
-import deleteList from '../helpers/deleteList'
+import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import CardList from "./CardList";
+import addCardList from "../helpers/addCardList";
+import deleteList from "../helpers/deleteList";
 
-const DndContext = (props) => {
-
+const DndContext = props => {
   return (
-    <DragDropContext
-      onDragEnd={props.onDragEnd}>
-
-      <div className='days-container'>
-      {Object.entries(props.columns).map(([columnId, column]) => {
-        if(columnId !== 'list'){
-          return (
-            <div
-            className='column-container'
-              key={columnId}
-            >
-              <div className='column-name-container'>
-                <h3 className='column-name' >{column.name}</h3>
-                {columnId!=='list' && column.name!=='Day 1'? 
-                <button onClick={()=>deleteList(columnId, props.columns, props.setColumns)} type="button" className="delete-list btn btn-danger"><i className="material-icons">
-                delete
-                </i></button>:""}
+    <DragDropContext onDragEnd={props.onDragEnd}>
+      <div className="days-container">
+        {Object.entries(props.columns).map(([columnId, column]) => {
+          if (columnId !== "list") {
+            return (
+              <div className="column-container" key={columnId}>
+                <div className="column-name-container">
+                  <h3 className="column-name">{column.name}</h3>
+                  {columnId !== "list" && column.name !== "Day 1" ? (
+                    <button
+                      onClick={() =>
+                        deleteList(columnId, props.columns, props.setColumns)
+                      }
+                      type="button"
+                      className="delete-list btn btn-danger"
+                    >
+                      <i className="material-icons">delete</i>
+                    </button>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="outside-column">
+                  <CardList
+                    droppableId={columnId}
+                    key={columnId}
+                    columnId={columnId}
+                    column={column}
+                    columns={props.columns}
+                    setColumns={props.setColumns}
+                    totalCost={props.totalCost}
+                    budget={props.budget}
+                    setHoverActivity={props.setHoverActivity}
+                  />
+                </div>
               </div>
-              <div className='outside-column'>
-                <CardList 
-                droppableId={columnId} 
-                key={columnId}
-                columnId={columnId}
-                column={column}
-                columns={props.columns}
-                setColumns={props.setColumns}
-                totalCost={props.totalCost}
-                // setTotalCost={props.setTotalCost}
-                budget={props.budget}
-                setHoverActivity={props.setHoverActivity}
-                />
-              </div>
-            </div>
-          );
-        }
-        return null;
-      })}
-      <div className='add-list-container'>
-        <button type="button" className="btn btn-primary add-list" onClick={(()=>{
-        addCardList(props.columns, props.setColumns)
-        })} ><i className="material-icons"> add</i>
-        </button>
+            );
+          }
+          return null;
+        })}
+        <div className="add-list-container">
+          <button
+            type="button"
+            className="btn btn-primary add-list"
+            onClick={() => {
+              addCardList(props.columns, props.setColumns);
+            }}
+          >
+            <i className="material-icons"> add</i>
+          </button>
+        </div>
       </div>
-      </div>
-        <div
-        className='column-container activity-list'
-        >
-          {/* <h2>{props.columns['list'].name}</h2> */}
-          <div className='outside-column'>
-            <CardList 
-            droppableId='list'
-            columnId={'list'}
-            column={props.columns['list']}
+      <div className="column-container activity-list">
+        {/* <h2>{props.columns['list'].name}</h2> */}
+        <div className="outside-column">
+          <CardList
+            droppableId="list"
+            columnId={"list"}
+            column={props.columns["list"]}
             columns={props.columns}
             setColumns={props.setColumns}
             totalCost={props.totalCost}
-            // setTotalCost={props.setTotalCost}
             budget={props.budget}
             setHoverActivity={props.setHoverActivity}
-            />
-          </div>
-    
+          />
         </div>
-
-       
-
-   </DragDropContext>
+      </div>
+    </DragDropContext>
   );
 };
 
