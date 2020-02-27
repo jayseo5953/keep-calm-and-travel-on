7c1abbedcce6f31da4e1d8ea77858eb73c95472b
tausvels <engr.tausifkhan@gmail.com>
2020-02-27 13:32:37 -0800

    cleaned searchbar comp

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -28,8 +28,7 @@ export default function SearchBar (props) {
           InputLabelProps={{
             style: { color: '#fff', fontSize: "16px", fontFamily: 'Lobster'},
           }}
-          InputProps={{style: {color: '#fff', fontSize: '20px', fontFamily: 'Lobster' } }}
-          input 
+          InputProps={{style: {color: '#fff', fontSize: '20px', fontFamily: 'Lobster' } }} 
           id="standard-search" 
           label="Enter location or budget" 
           type='text'
@@ -54,50 +53,3 @@ export default function SearchBar (props) {
   )
 
 }
-
-// <div style={{width: 300}}>
-      
-// <Autocomplete
-//   id="combo-box-demo"
-//   options={cities.map(city => city.city)}
-//   // getOptionLabel={option => option.title}
-//   style={{ width: 300 }}
-//   renderInput={params => (
-//     <TextField {...params} label="comboBox" margin="normal" variant="outlined" fullWidth onChange={(event)=>getCities(event.target.value, setCities)} />
-// )}
-// />
-// </div>
-
-
-
-
-// import React from 'react';
-// import TextField from "@material-ui/core/TextField";
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: 500,
-//     },
-//   },
-// }));
-
-// export default function SearchBar (props) {
-//   const classes = useStyles()
-//   return(
-//     // <form>
-//     //   <TextField placeholder="Destination, Budget or Activity" onChange={(event)=> props.onChange(event.target.value)}/>
-//     // </form>
-//     <form className={classes.root} noValidate autoComplete="off">
-//     <TextField 
-//     id="standard-search" 
-//     label="Enter your destination, budget or activity" 
-//     type="search" 
-//     onChange={(event)=> props.onChange(event.target.value)}
-//     />
-//     </form>
-//   )
-// }
-
